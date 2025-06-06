6c0507decf5257a84610b02f22e90c315087e794
Geert Meersman <geertmeersman+github@gmail.com>
2025-06-06 14:01:44 +0200

    fix: environment variables cron fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Dump selected environment variables with quoting
+{
+  printenv | grep -E '^(PRINTER_URI|PRINTER_NAME|SMTP_USER|SMTP_PWD|SMTP_SERVER|SMTP_PORT|EMAIL_FROM|EMAIL_TO|TELEGRAM_BOT_ID|TELEGRAM_CHAT_ID)=' | \
+    while IFS='=' read -r key value; do
+      echo "export ${key}=\"${value//\"/\\\"}\""
+    done
+} > /env.sh
+chmod +x /env.sh
+
+
 # Start CUPS daemon in the background
 cupsd &
 
